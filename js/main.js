9b23989d1c6f7aac81d74e57a95879764912a1af
bdrobniuch <drobniuch@gmail.com>
2018-10-04 20:52:55 +0200

    koniec

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/*
+Stwórz plik "main.js".
+
+Zdeklaruj zmienną o dowolnej nazwie i przypisz do niej tablicę składającą się z 5 liczb.
+
+Napisz funkcję, która przyjmuje tablicę jako parametr. Funkcja ma zwracać pierwszy element tablicy.
+Uruchom funkcję, a wynik jej działania wyświetl w konsoli.
+
+Plik wgraj do zdalnego repozytorium i podeślij link do sprawdzenia.
+*/
+
+
+var t = [1,2,3,4,5];
+
+function first(tab)
+{
+    return tab[0];
+}
+
+console.log(first(t));
+
